c98dacf263bcf952f12be987361050b9f16e2422
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-29 10:33:17 -0400

    begin clock layout

--- src/js/components/SimpleCounter.jsx
+++ src/js/components/SimpleCounter.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { FaRegClock } from 'react-icons/fa6';
 
 const SimpleCounter = () => {
-  return <div>SimpleCounter</div>;
+  return (
+    <div className="counter-wrapper">
+      <div className="cal-icon">
+        <FaRegClock />
+      </div>
+      <div className="digit-4"></div>
+      <div className="digit-3"></div>
+      <div className="digit-2"></div>
+      <div className="digit-1"></div>
+    </div>
+  );
 };
 
 export default SimpleCounter;
