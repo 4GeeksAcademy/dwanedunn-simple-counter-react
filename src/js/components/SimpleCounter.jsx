580e065a78936b10d2fe89a32c19385681da8010
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-29 10:47:36 -0400

    style counter display

--- src/js/components/SimpleCounter.jsx
+++ src/js/components/SimpleCounter.jsx
@@ -3,14 +3,14 @@ import { FaRegClock } from 'react-icons/fa6';
 
 const SimpleCounter = () => {
   return (
-    <div className="counter-wrapper">
+    <div className="bg-dark text-white counter-wrapper d-flex align-items-center justify-content-center p-2">
       <div className="cal-icon">
         <FaRegClock />
       </div>
-      <div className="digit-4"></div>
-      <div className="digit-3"></div>
-      <div className="digit-2"></div>
-      <div className="digit-1"></div>
+      <div className="digit-4 p-1">0</div>
+      <div className="digit-3 p-1">0</div>
+      <div className="digit-2 p-1">0</div>
+      <div className="digit-1 p-1">1</div>
     </div>
   );
 };
