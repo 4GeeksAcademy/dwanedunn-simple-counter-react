ab3117d3d5c34742ba15b209d08b6577f329ae00
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-29 12:26:00 -0400

    clean up counter function

--- src/js/components/SimpleCounter.jsx
+++ src/js/components/SimpleCounter.jsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import { FaRegClock } from 'react-icons/fa6';
 
-let Counter = 0;
-setInterval(() => {
-  const four = Math.floor(Counter / 1000);
-  const three = Math.floor(Counter / 100);
-  const two = Math.floor(Counter / 10);
-  const one = Math.floor(Counter / 1);
-  console.log('one:', one, 'two:', two, 'three:', three, 'four:', four);
-}, 1000);
-
 const SimpleCounter = (props) => {
   return (
     <div className="bg-dark text-white counter-wrapper d-flex align-items-center justify-content-center p-2">
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 //Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap';
+// import 'bootstrap';
 
 // index.css'
 import '../styles/index.css';
@@ -11,8 +11,23 @@ import '../styles/index.css';
 // components
 import SimpleCounter from './components/SimpleCounter';
 
+let Counter = 0;
+setInterval(function () {
+  const four = Math.floor(Counter / 1000);
+  const three = Math.floor(Counter / 100);
+  const two = Math.floor(Counter / 10);
+  const one = Math.floor(Counter / 1);
+  console.log('one:', one, 'two:', two, 'three:', three, 'four:', four);
+  Counter++;
+}, 1000);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SimpleCounter />
+    <SimpleCounter
+      digitOne={one}
+      digittwo={two}
+      digitThree={three}
+      digitFour={four}
+    />
   </React.StrictMode>
 );
