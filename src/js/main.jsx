d93a368e38e7cffb6a5c1b19db432262c2ec1ae5
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-29 13:27:45 -0400

    comment setInterval

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -16,29 +16,13 @@ let one = 0;
 let two = 0;
 let three = 0;
 let four = 0;
-
+// update the digits split up into four variables
 setInterval(function () {
   four = Math.floor(Counter / 1000) % 10;
   three = Math.floor(Counter / 100) % 10;
   two = Math.floor(Counter / 10) % 10;
   one = Math.floor(Counter / 1) % 10;
 
-  // one = one % 10;
-  // two = two % 10;
-  // three = three % 10;
-  // four = four % 10;
-  console.log(
-    'Counter',
-    Counter,
-    'one',
-    one,
-    'two',
-    two,
-    'three',
-    three,
-    'four',
-    four
-  );
   Counter++;
 }, 1000);
 
