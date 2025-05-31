ebb46a46257bf0b819ac70c77306d16d80bfe501
Dwane Dunn <dwane.dunn@gmail.com>
2025-05-31 16:01:02 -0400

    add border to digits

--- src/js/components/SimpleCounter.jsx
+++ src/js/components/SimpleCounter.jsx
@@ -5,18 +5,18 @@ const SimpleCounter = ({ digitFour, digitThree, digitTwo, digitOne }) => {
   return (
     <div className="bg-dark text-white counter-wrapper d-flex align-items-center justify-content-center p-2">
       <div className="cal-icon">
-        <FaRegClock />
+        <FaRegClock className="border border-1 border-black" />
       </div>
-      <div className="digit-4 p-1 border border-1 border-secondary-subtle rounded">
+      <div className="digit-4 p-1 border border-1 border border-black rounded">
         {digitFour}
       </div>
-      <div className="digit-3 p-1 border border-1 border-secondary-subtle rounded">
+      <div className="digit-3 p-1 border border-1 border border-black rounded">
         {digitThree}
       </div>
-      <div className="digit-2 p-1 border border-1 border-secondary-subtle rounded">
+      <div className="digit-2 p-1 border border-1 border border-black rounded">
         {digitTwo}
       </div>
-      <div className="digit-1 p-1 border border-1 border-secondary-subtle rounded">
+      <div className="digit-1 p-1 border border-1 border border-black rounded">
         {digitOne}
       </div>
     </div>
